79851e22065ffda8cc1d0d709482b101cbf4be98
Yazin Tantawi <yazinmedhattantawi@gmail.com>
2025-04-30 16:37:14 +0300

    Updated ganamede.h, added "show value at address" feature

--- ganamede.h
+++ ganamede.h
@@ -1,18 +1,18 @@
 // Hello from the Aspen Software Foundation!
-// Hi there! Founder speaking, I hope all of you have a great time with Ganamede!
 // This file is part of the Aspen Ganamede Leak Checker (AGLC).
 
-   //  _    ____  ____  _____ _   _ 
+    // _    ____  ____  _____ _   _ 
    // / \  / ___||  _ \| ____| \ | |
   // /   \ \___ \| |_) |  _| |  \| |
  // /     \ ___) |  __/| |___| |\  |
 // /       \____/|_|   |_____|_| \_|
 
 #ifndef GANAMEDE_H
-#define GANAMIDE_H
+#define GANAMEDE_H
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 typedef struct GanamedeBlock {
     void* ptr;
@@ -28,7 +28,7 @@ static GanamedeBlock* __ganamede_head = NULL;
 static void __ganamede_add_block(void* ptr, size_t size, const char* file, int line) {
     printf("Allocating memory: %zu bytes at %p (file: %s, line: %d)\n", size, ptr, file, line);
 
-    GanamedeBlock* block = (GanamedeBlock*)malloc(sizeof(GanamedeBlock)); // Explicit cast here
+    GanamedeBlock* block = (GanamedeBlock*)malloc(sizeof(GanamedeBlock));
     block->ptr = ptr;
     block->size = size;
     block->file = file;
@@ -40,7 +40,7 @@ static void __ganamede_add_block(void* ptr, size_t size, const char* file, int l
 // Remove a block when memory is freed
 static void __ganamede_remove_block(void* ptr) {
     printf("Freeing memory: %p\n", ptr);
-    
+
     GanamedeBlock** curr = &__ganamede_head;
     while (*curr) {
         if ((*curr)->ptr == ptr) {
@@ -53,7 +53,7 @@ static void __ganamede_remove_block(void* ptr) {
     }
 }
 
-// Wrapped malloc function with tracking
+// Tracked malloc
 static void* __ganamede_tracked_malloc(size_t size, const char* file, int line) {
     void* ptr = malloc(size);
     if (ptr) {
@@ -62,13 +62,13 @@ static void* __ganamede_tracked_malloc(size_t size, const char* file, int line) 
     return ptr;
 }
 
-// Wrapped free function with tracking
+// Tracked free
 static void __ganamede_tracked_free(void* ptr) {
     __ganamede_remove_block(ptr);
     free(ptr);
 }
 
-// This function will check for any memory leaks at the end of the program
+// Leak dump with 64-byte display
 static void _CrtDumpMemoryLeaks() {
     if (__ganamede_head == NULL) {
         printf("No memory leaks detected.\n");
@@ -78,12 +78,31 @@ static void _CrtDumpMemoryLeaks() {
         while (curr) {
             printf(" Leak: %zu bytes at %p (allocated at %s:%d)\n",
                    curr->size, curr->ptr, curr->file, curr->line);
+
+            size_t to_display = curr->size < 64 ? curr->size : 64;
+            uint8_t* bytes = (uint8_t*)curr->ptr;
+
+            printf("  Contents (up to 64 bytes):\n   HEX : ");
+            for (size_t i = 0; i < to_display; i++) {
+                printf("%02X ", bytes[i]);
+                if ((i + 1) % 16 == 0 && i + 1 < to_display)
+                    printf("\n         ");
+            }
+
+            printf("\n  ASCII: ");
+            for (size_t i = 0; i < to_display; i++) {
+                printf("%c", (bytes[i] >= 32 && bytes[i] <= 126) ? bytes[i] : '.');
+                if ((i + 1) % 16 == 0 && i + 1 < to_display)
+                    printf("\n         ");
+            }
+
+            printf("\n\n");
             curr = curr->next;
         }
     }
 }
 
-// Automatically hook malloc and free without needing definitions
+// Auto-override malloc/free when included
 #define malloc(x) __ganamede_tracked_malloc(x, __FILE__, __LINE__)
 #define free(x)   __ganamede_tracked_free(x)
 
